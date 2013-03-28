e040c960d0678d234595b58595157e90400d549b
Pallan Madhavan <mady902@gmail.com>
2013-03-29 02:09:37 +0530

    applying the dis module

---
+++ dis_module.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import dis
+
+def sum():
+   vara = 10
+   varb = 20
+
+   sum = vara + varb
+   print "vara + varb = %d" % sum
+
+# Call dis function for the function.
+
+dis.dis(sum)
